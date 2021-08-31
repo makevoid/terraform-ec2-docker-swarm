1a6c1059222ec10b3335ad72139773c4ab335988
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2021-08-31 08:48:47 +0100

    Update env.rb

--- env.rb
+++ env.rb
@@ -4,7 +4,6 @@ require 'yaml'
 
 require_relative 'lib/cmd_lib'
 require_relative 'lib/lib'
-require_relative 'lib/deployer_config'
 
 PATH = File.expand_path "../", __FILE__
 
@@ -17,26 +16,5 @@ IP_VM_B = "10.1.4.4"
 
 DEBUG = true
 
-PROVISIONER_GIT_URI = "git@github.com:appliedblockchain/provisioner" # development - Provisioner (VM provisioner) is open source already
-PROVISIONER_BRANCH = "master"
-
-# AB's AWS account default VPC
-AWS_DEFAULT_VC = ""
-
-DEPLOYER_HOST = "deployer.abtech.dev"
-
-DEPLOYER_CONFIG_GIT_URI = "git@github.com:makevoid/deployer-config" # NOTE: not open source yet
-
-DEPLOYER_CONFIG_STACK_CONF = {
-  stack_name: "docker-app",
-  github_repo: "docker-app",
-  containers: [
-    "docker-app",
-    "nginx",
-    "redis",
-  ]
-}
-
 include CmdLib
 include Lib
-include DeployerConfig
